dc6f1ec15dc16b184ce813e355d51ccdc6dc7fee
Juang, Yi-Lin <frankyjuang@gmail.com>
2016-12-19 16:06:24 +0100

    Add test

---
+++ src/test_score_pos_mapping.py
@@ -0,0 +1,24 @@
+#! /usr/bin/env python
+
+import time
+
+import rospy
+
+import detection
+import score
+
+rospy.init_node('test', anonymous=True)
+detector = detection.Detection()
+scorer = score.Score()
+f = open("data1","w+")
+
+while True:
+    raw_input()
+    score = scorer.send(detector.captured_image, detector.camera_info)
+    f.write(str(score) + "\n")
+    pos = detector.detection_data.detections[0].pose.pose.position
+    ori = detector.detection_data.detections[0].pose.pose.orientation
+    f.write(str(pos) + "\n")
+    f.write(str(ori) + "\n\n")
+    f.flush()
+f.close()

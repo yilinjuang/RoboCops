3fd1452078ed0a068edf9e95b967c4779fd2d2a2
Juang, Yi-Lin <frankyjuang@gmail.com>
2017-01-11 17:18:18 +0100

    Minor fix of test

--- src/test_score_pos_mapping.py
+++ src/test_score_pos_mapping.py
@@ -12,12 +12,17 @@ detector = detection.Detection()
 scorer = score.Score()
 f = open("data1","w+")
 
-while True:
+while not rospy.is_shutdown():
     raw_input()
     score = scorer.send(detector.captured_image, detector.camera_info)
+    # amount = len(detector.detection_data)
+    # print("=" * 20)
+    # print(score)
+    # print(amount)
+    # print("=" * 20)
     f.write(str(score) + "\n")
-    pos = detector.detection_data.detections[0].pose.pose.position
-    ori = detector.detection_data.detections[0].pose.pose.orientation
+    pos = detector.detection_data[0].pose.pose.position
+    ori = detector.detection_data[0].pose.pose.orientation
     f.write(str(pos) + "\n")
     f.write(str(ori) + "\n\n")
     f.flush()

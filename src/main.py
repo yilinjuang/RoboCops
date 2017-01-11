4658aaf76da3c2c584741382d87b556dc90cc0a2
Juang, Yi-Lin <frankyjuang@gmail.com>
2017-01-11 17:20:03 +0100

    Combine most strategies into main program

--- src/main.py
+++ src/main.py
@@ -3,6 +3,7 @@
 import math
 import random
 import sys
+from timeit import default_timer as timer
 
 import rospy
 
@@ -20,13 +21,35 @@ class Main:
         self.scorer = score.Score()
 
         self.rate = rospy.Rate(10)
+        self.TO_SHOOT_OR_NOT_TO_SHOOT = 20
+        self.cool_down = timer() - 15
 
-    def action(self):
-        # goal = raw_input()
-        # self.mover.explore()
-        # self.mover.move_to(goal)
+    def run(self):
         pos = self.mover.get_position()
-        print(pos)
+        print("Position: " + pos)
+
+        if not self.detector.detected:
+            # Explore!
+            print("===Explore===")
+            self.mover.explore()
+        else:
+            if timer() - self.cool_down < 10:
+                # After shooting!
+                print("===CoolDown(Explore)===")
+                self.mover.explore()
+            else:
+                if self.detector.best_score > self.TO_SHOOT_OR_NOT_TO_SHOOT:
+                    # Shoot!
+                    print("===Shoot===")
+                    score = self.scorer.send(self.detector.captured_image,
+                            self.detector.camera_info)
+                    if score > 0:
+                        # Shot successfully.
+                        self.cool_down = timer()
+                else:
+                    # Follow!
+                    print("===Follow===")
+                    self.mover.follow(self.detector.best_position)
 
         self.rate.sleep()
 
@@ -37,6 +60,6 @@ if __name__ == '__main__':
     try:
         main = Main()
         while not rospy.is_shutdown():
-            main.action()
+            main.run()
     except rospy.ROSInterruptException:
-        print("program interrupted before completion")
+        print("Program interrupted before completion.")

37e8c2aac7b1874959ea4a6d568f0cd6b58d36ec
Juang, Yi-Lin <frankyjuang@gmail.com>
2017-01-11 17:19:17 +0100

    Add score estimation

--- src/detection.py
+++ src/detection.py
@@ -3,6 +3,7 @@
 import apriltags_ros
 import rospy
 from apriltags_ros.msg import AprilTagDetectionArray
+from geometry_msgs.msg import Point
 from sensor_msgs.msg import CameraInfo, CompressedImage
 
 class Detection:
@@ -12,9 +13,69 @@ class Detection:
         self.camera_info = CameraInfo()
         self.detection_data = AprilTagDetectionArray()
 
+        self.best_score = -1
+        self.best_idx = -1
+        self.best_position = Point()
+
+        # Register listeners.
         self.detect()
         self.capture()
         self.info()
+
+    def estimate_score(self):
+        """Estimate score of the detected tag |self.detection_data|.
+
+        score = orient + dist + center
+
+        """
+        best_score = -1
+        best_idx = -1
+        best_position = Point()
+        for idx, data in enumerate(self.detection_data):
+            # TODO: use more accurate value range of score especially orient.
+            pos = data.pose.pose.position
+            orient = 5
+
+            if pos.z < 0.5:
+                dist = 10
+            elif pos.z < 0.8:
+                dist = 9
+            elif pos.z < 1.1:
+                dist = 8
+            else:
+                dist = 7
+
+            if pos.x < 0.07:
+                center = 10
+            elif pos.x < 0.10:
+                center = 9
+            elif pos.x < 0.12:
+                center = 8
+            elif pos.x < 0.15:
+                center = 7
+            elif pos.x < 0.17:
+                center = 6
+            elif pos.x < 0.20:
+                center = 5
+            elif pos.x < 0.22:
+                center = 4
+            elif pos.x < 0.24:
+                center = 3
+            elif pos.x < 0.27:
+                center = 2
+            elif pos.x < 0.33:
+                center = 1
+            else:
+                center = 0
+
+            score = orient + dist + center
+            if score > best_score:
+                best_score = score
+                best_idx = idx
+                best_position = pos
+        self.best_score = best_score
+        self.best_idx = best_idx
+        self.best_position = best_position
 
     def get_detect_data(self, data):
         """Callback function of detect().
@@ -31,11 +92,8 @@ class Detection:
 
             print("Detected!")
             self.detected = True
-            self.detection_data = data
-            pos = data.detections[0].pose.pose.position
-            ori = data.detections[0].pose.pose.orientation
-            # print(str(pos))
-            # print(str(ori))
+            self.detection_data = data.detections
+            self.estimate_score()
 
     def get_capture_data(self, data):
         """Callback function of capture().
